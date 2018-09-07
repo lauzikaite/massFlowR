9edbdb0f37310a7377cfa222fc92af37bc59c9bd
lauzikaite <elze.lauzikaite@gmail.com>
2018-09-07 09:26:42 +0100

    simplifying checkSPIKED.R functions to plot any selected peaks network and correlated peaks spectra, pair-wise.

--- R/functions-checkSPIKED.R
+++ R/functions-checkSPIKED.R
@@ -131,9 +131,9 @@ checkSPIKED <- function(fname, pks, raw, spk, out_dir, paramCWT, add_xcmsparams 
 
 
 ## check list pf pre-selected peaks (can be all from one COMP, can be simply co-eluting, can be from same CLS)
-## build correlatio  network
+## build correlation  network
 ## for selected pairs of the network, plot spectra and output cor values and MZ differences
-checkPEAKS <- function(pks_mat, pno_main, eic, raw, out_dir, paramCWT, add_xcmsparams = F, thr = 0.95, use_thr = T) {
+checkPEAKS <- function(pks_mat, spiked, spiked_p, eic, raw, out_dir, paramCWT, add_xcmsparams = F, thr = 0.95, use_thr = T) {
 
   ## get cor values between all co-eluting peaks, pair-wise
   pks_mat <- pks_mat %>%
@@ -159,6 +159,16 @@ checkPEAKS <- function(pks_mat, pno_main, eic, raw, out_dir, paramCWT, add_xcmsp
       filter(pno %in% pno_all)
   }
 
+  buildNETWORK(poi_co_cor = pks_cor,
+               pkscomps = pks_mat,
+               co_ind = pno_all,
+               thr = thr,
+               pks = pks_mat,
+               p = spiked_p,
+               plot = T,
+               out_dir = out_dir,
+               fname = fname)
+
   ## for every peak-pair, extract cor and mz_diff
   pks_cor_mz <- pks_cor %>%
     group_by(x, y) %>%
@@ -174,30 +184,48 @@ checkPEAKS <- function(pks_mat, pno_main, eic, raw, out_dir, paramCWT, add_xcmsp
   ## extract EIC values for every pno
   spec <- pks_mat %>%
     group_by(pno) %>%
-    do(extractSPECTRUM(co = ., raw = raw))
+    do(extractSPECTRUM(co = ., raw = raw)) %>%
+    ungroup()
+
+  ## plot all peaks in one
+  g <- ggplot(data = spec) +
+    geom_segment(aes(x = rtime, xend = rtime, y = 0, yend = intensity, colour = as.factor(pno)), na.rm = TRUE) +
+    geom_point(aes(x = rtime, y = intensity, colour = as.factor(pno)), na.rm = TRUE) +
+    scale_color_viridis_d(name = "Peak ID", end = 0.9) +
+    ylab("Intensity") +
+    xlab("Retention time") +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(legend.position = "bottom")
+
+  grid::grid.newpage()
+  pdf(width = 10, height = 10,
+      file = paste0(out_dir, "/", fname, "_peaks-",paste0(pks_mat$pno, collapse = "-"),  ".pdf"))
+  grid::grid.draw(rbind(ggplotGrob(g), size = "last"))
+  dev.off()
 
   g <- pks_cor_mz %>%
     group_by(x,y) %>%
-    filter(x == 4, y ==1) %>%
     mutate(gtitle = paste0("Cor: ", round(cor, digits = 2), ". MZ diff: ", mz_dif)) %>%
-    do(plotPEAKpair(pair = ., spec = spec, add_xcmsparams = T, paramCWT = paramCWT))
+    do(plotPEAKpair(pair = ., spec = spec, spiked = spiked, add_xcmsparams = T, paramCWT = paramCWT, out_dir = out_dir, fname = fname))
 
-
-
-
-
-
+ return(pks_cor_mz)
 }
 
-plotPEAKpair <- function(pair, spiked, spec, add_xcmsparams, paramCWT){
+plotPEAKpair <- function(pair, spiked, spec, add_xcmsparams, paramCWT, out_dir, fname){
 
   ## extract pair of interest
   spec <- spec %>%
     filter(pno == pair$x | pno == pair$y)
 
+  if (spiked == T) {
+    spec <- spec %>%
+      mutate(pno = ifelse(pno == spiked_p, "spiked", pno))
+  }
+
   cols <- setNames(viridis::viridis(end = 0.8, alpha = 0.8,
                                     n = length(unique(spec$pno))),
-                   nm = if (spiked == T) {
+                   nm = if (spiked == T & "spiked" %in% spec$pno) {
                      c("spiked", spec %>% filter(pno != "spiked") %>% distinct(pno) %>% pull()) } else {
                        c(spec %>% filter(pno != "spiked") %>% distinct(pno) %>% pull()) }
   )
@@ -225,177 +253,14 @@ plotPEAKpair <- function(pair, spiked, spec, add_xcmsparams, paramCWT){
                             guide = guide_legend(override.aes = list(color = c("#762a83", "#1b7837"))))
   }
 
-  return(g)
+  grid::grid.newpage()
+  pdf(width = 10, height = 10,
+      file = paste0(out_dir, "/", fname, "_peakPair-",pair$x, "-", pair$y, ".pdf"))
+  grid::grid.draw(rbind(ggplotGrob(g), size = "last"))
+  dev.off()
 
+  return(spec)
 }
-
-
-
-
-checkSPIKEDcomps <- function(i, fname, pks, raw, eic, spk_pks, cor_mat, out_dir, paramCWT, rt_err = 3, mz_err = 0.005, thr = 0.95, pearson = TRUE) {
-
-  if (missing(i)) {
-    ## for all spiked MR compounds
-    i_list <- 1:nrow(spk_pks)
-
-  } else {
-    ## for selected spiked MR compound only
-    i_list <- i
-  }
-
-  pks <- pks %>%
-    ## rename pid to pno, if old-version table is being used
-    rename(pno = matches("pid|pno"))
-
-  cor_data <- data.frame()
-  for(i in i_list) {
-    message("Checking spiked compound: ", spk_pks[i,"spiked_id"], ", i = ", i)
-
-    ## get the peak ID which represents this compound
-    poi <- spk_pks[i,"pks_id"]
-    cor_data_i <- checkANYpeak(p_list = poi, fname = fname, pks = pks, raw = raw, eic = eic, out_dir = out_dir, paramCWT = paramCWT, use_spk = T, use_thr = T, add_xcmsparams = F)
-
-    ## get mz difference to the absolute spiked MZ
-    cor_data_i  <- data.frame(
-      mz_molw_dif = spk_pks[i,"spiked_molw"] - (cor_data_i %>% distinct(mz_x, mz_y) %>% pull()),
-      mz_m_dif = spk_pks[i,"spiked_mz"] - (cor_data_i %>% distinct(mz_x, mz_y) %>% pull()),
-      stringsAsFactors = F) %>%
-      mutate(spiked_i = i)
-
-    cor_data <- rbind(cor_data, cor_data_i)
-  }
-
-  return(cor_data)
-}
-
-
-checkANYpeak <- function(p_list, fname, pks, raw, eic, out_dir, paramCWT, use_spk, use_thr = T, thr = 0.95, add_xcmsparams = F) {
-
-  cor_data <- data.frame()
-
-  for(p in p_list) {
-
-    message("Checking peak with pno: ", p )
-
-    ## get component peaks for the poi
-    p_comp <- pks[p, "comp"]
-    p_co <- pks %>%
-      filter(comp == p_comp) %>%
-      mutate(poi = ifelse(pno == p, T, F)) %>%
-      ## main peak - the spiked one, or the most intense
-      mutate(main =  if (use_spk == T) { ifelse(pno == p, T, F) } else { ifelse(into == max(into), T, F) })
-
-    if (nrow(p_co) == 1) { next("No co-eluting peaks for PID: ", p) }
-
-    ## get cor values between all co-eluting peaks, pair-wise
-    p_co_pno <- pull(p_co, pno)
-    p_co_cor <- buildCOR(co_ind = p_co_pno, eic = eic, pearson = T)
-
-    ## if only peaks above cor threshold is desired
-    if (use_thr == T) {
-      p_co_cor <- p_co_cor %>%
-        filter(cor > thr)
-      p_co <- p_co %>%
-        filter(pno %in%
-                 (p_co_cor %>%
-                 select(x,y) %>%
-                 pull()))
-      }
-
-    ## extract EIC values for every corelated peak
-    spec <- p_co %>%
-      group_by(pno) %>%
-      do(extractSPECTRUM(co = ., raw = raw)) %>%
-      # mutate(co_pno = pno) %>%
-      # mutate(poi = poi) %>%
-      # mutate(main = main) %>%
-      ungroup()
-
-    ##  save MZ differences within the component
-    p_co_cor <- p_co_cor %>%
-      group_by(x, y) %>%
-      mutate(x_y = paste(min(c(x, y)), max(c(x, y)), sep = "_")) %>%
-      group_by(x_y) %>%
-      slice(1) %>%
-      mutate(mz_x = pks %>% filter(pno == x) %>% pull(mz),
-             mz_y = pks %>% filter(pno == y) %>% pull(mz)) %>%
-      mutate(mz_dif = round(abs(mz_x - mz_y), digits = 2)) %>%
-      ungroup()
-
-    ## save POI-co peak pairs
-    cor_data <- rbind(cor_data,
-                      p_co_cor %>%
-                        filter(x == p | y == p))
-
-    ####---- plot EICs of main adduct and all co-eluting peaks
-    ## only works well if peaks are only correlated with the POI
-    ## need to re-write for more generic cases
-    # g_labels <- p_co_cor %>%
-    #   filter(x == p | y == p) %>%
-    #   # mutate(not_p_pno = ifelse(x == p, y, x)) %>%
-    #   # group_by(not_p_pno) %>%
-    #   # slice(1) %>%
-    #   mutate(g_text = paste0("Cor: ", round(cor, digits = 2), ". MZ diff: ", mz_dif)) %>%
-    #   ungroup() %>%
-    #   select(x, y, not_p_pno, g_text) %>%
-    #   bind_rows(data.frame(x = p, y = p, not_p_pno = p, g_text = paste("Peak-of-Interest"), stringsAsFactors = F)) %>%
-    #   arrange(not_p_pno)
-    #
-    # g <- ggplot(spec, aes(x = rtime, xend = rtime, y = 0, yend = intensity)) +
-    #   ## exclusion of column 'co_pid' allows to retain data points in each facet
-    #   geom_segment(data = filter(spec, main == T) %>% select(., -co_pno),
-    #                alpha = 0.6, colour = "black", na.rm = TRUE) +
-    #   geom_point(data = filter(spec, main == T)  %>% select(., -co_pno), aes(y = intensity),
-    #              alpha = 0.6, colour = "black", na.rm = TRUE) +
-    #   geom_segment(data = filter(spec, main == F),
-    #                aes(colour = as.factor(co_pno)),
-    #                alpha = 0.8, na.rm = TRUE) +
-    #   geom_point(data = filter(spec, main == F),
-    #              aes(y = intensity,colour = as.factor(co_pno)),
-    #              alpha = 0.8, na.rm = TRUE) +
-    #   scale_color_viridis_d(end = 0.8,
-    #                         alpha = 0.8,
-    #                         name = "Peak ID") +
-    #   facet_wrap(~co_pno,
-    #              labeller =  as_labeller(setNames(g_labels$g_text, nm = g_labels$not_p_pno)),
-    #              scales = "free_y") +
-    #   xlab("Retention time") +
-    #   ylab("Intensity") +
-    #   theme_bw() +
-    #   theme(legend.position = "bottom")
-    #
-    # if (add_xcmsparams == T) {
-    #   g <- g +
-    #     geom_hline(aes(yintercept = paramCWT@prefilter[2],
-    #                    linetype = paste0("Prefilter: c(", paramCWT@prefilter[1], "," , paramCWT@prefilter[2], ")")),
-    #                colour= "#1b7837") +
-    #     geom_hline(aes(yintercept = paramCWT@noise,
-    #                    linetype = paste("Noise:", paramCWT@noise)),
-    #                colour= '#762a83') +
-    #     scale_linetype_manual(name = "centWave parameters", values = c(2, 2),
-    #                           guide = guide_legend(override.aes = list(color = c("#762a83", "#1b7837"))))
-    #   }
-    #
-    #   grid::grid.newpage()
-    #   pdf(width = 10, height = 10,  paper = "a4",
-    #       file = paste0(out_dir, "/", fname, "_poi-", p, "_allCO_spectra.pdf"))
-    #   grid::grid.draw(ggplotGrob(g))
-    #   dev.off()
-
-
-  }
-
-  return(cor_data)
-}
-
-
-
-
-
-
-
-
-
 
 #' Extract EIC for a peak-of-interest using its range of mz and rt values
 #'
